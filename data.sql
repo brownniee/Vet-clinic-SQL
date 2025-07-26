8b023981da241a4c959cafb8db76bedf265a548a
mac <mac@Felicias-MacBook-Air.local>
2025-07-27 01:21:46 +0530

    values inserted into the table

--- data.sql
+++ data.sql
@@ -1,9 +1,8 @@
-CREATE TABLE animals (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5,2)
-);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES 
+  ('Agumon', '2020-02-03', 0, TRUE, 10.23),
+  ('Gabumon', '2018-11-15', 2, TRUE, 8.00),
+  ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
+  ('Devimon', '2017-05-12', 5, TRUE, 11.00);
+  
 
--- schema.sql
+++ schema.sql
@@ -1,9 +1,8 @@
 CREATE TABLE animals (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5,2)
+  id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+  name VARCHAR(100) STRING NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INTEGER NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL(5,2) NOT NULL
 );
-
