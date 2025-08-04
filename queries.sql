3c5646bba43abd1fddc322163d6885a8e3544b46
Odunayo Felicia Aremu <aremuodunayo98@gmail.com>
2025-08-04 11:22:40 +0530

    Queriied the table using transaction logic

--- queries.sql
+++ queries.sql
@@ -17,3 +17,62 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals 
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'unspecified';
+
+ROLLBACK;
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT before_weight_update;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT before_weight_update;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) AS avg_escapes
+FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
